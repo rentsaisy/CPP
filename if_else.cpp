88925b13cc162eaa68b3898d8632d8b77fcdfc7e
Currents <currentsaisyah@gmail.com>
2026-01-05 20:35:08 +0700

    chapter 6

---
+++ if_else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x, y, min;
+    cout << "Enter two different numbers:\n";
+    if( cin >> x && cin >> y)  // If both inputs are
+    {                          // valid, compute
+    if( x < y )              // the lesser.
+    min = x;
+    else
+    min = y;
+    cout << "\nThe smaller number is: " << min << endl;
+    }
+    else
+    cout << "\nInvalid Input!" << endl;
+    return 0;
+}
