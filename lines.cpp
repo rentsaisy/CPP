a8cf6847814491ff5dc6463ee61e12fefa4b9f4d
Currents <currentsaisyah@gmail.com>
2026-01-06 09:32:53 +0700

    chapter 7

---
+++ lines.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string line;
+    int number = 0;
+    while( getline( cin, line))   // As long as a line
+    {                             // can be read.
+    cout << setw(5) << ++number << ": "
+    << line << endl;
+    }
+    return 0;
+}
