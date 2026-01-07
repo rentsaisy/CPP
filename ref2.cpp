9d462cda7221d864f18d63facb170a4d3b641acf
Currents <currentsaisyah@gmail.com>
2026-01-08 06:54:29 +0700

    chapter 12

---
+++ ref2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// Prototypes:
+bool getClient( string& name, long& nr);
+void putClient( const string& name, const long& nr);
+int main()
+{
+string clientName;
+long   clientNr;
+cout << "\nTo input and output client data \n"
+<< endl;
+if( getClient( clientName, clientNr)) // Calls
+putClient( clientName, clientNr);
+else
+cout << "Invalid input!" << endl;
+return 0;
+}
+bool getClient( string& name, long& nr) 
+{
+// Definition
+cout << "\nTo input client data!\n"
+<< " Name:   ";
+if( !getline( cin, name))  return false;
+cout << " Number: ";
+if( !( cin >> nr))  return false;
+return true;
+}
+// Definition
+void putClient( const string& name, const long& nr)
+{                      // name and nr can only be read!
+cout << "\n-------- Client Data ---------\n"
+<< "\n Name:   ";  cout << name
+<< "\n Number: ";  cout << nr << endl;
+}
