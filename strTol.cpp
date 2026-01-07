216a38589ca7dab50bf6007738e3214ab069c50d
Currents <currentsaisyah@gmail.com>
2026-01-07 14:21:19 +0700

    chapter 11

---
+++ strTol.cpp
@@ -0,0 +1,22 @@
+#include <string>           // Type string
+#include <cctype>           // isspace() and isdigit()
+using namespace std;
+long strToLong( string str)
+{
+    register int i = 0;
+    // Index
+    long vz = 1, num = 0;         // Sign and number
+    // Ignore leading white spaces.
+    for(i=0; i < str.size() && isspace(str[i]); ++i)
+    ;
+    // Is there a sign?
+    if( i < str.size())
+    {
+    if( str[i] == '+' ) { vz = 1;  ++i; }
+    if( str[i] == '-' ) { vz = ---1; ++i; }
+    }
+    // Sequence of digits -> convert to integer
+    for( ; i < str.size() && isdigit(str[i]); ++i)
+    num = num * 10 + (str[i] - '0');
+    return vz * num;
+}
