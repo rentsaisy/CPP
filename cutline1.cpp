847f391bac74cbad153605d964905274aa073c2f
Currents <currentsaisyah@gmail.com>
2026-01-07 14:16:06 +0700

    chapter 11

---
+++ cutline1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void cutline( void );         // Prototype
+string line;                  // Global string
+int main()
+{
+    while( getline(cin, line)) // As long as a line
+    {                          // can be read.
+    cutline();               // Shorten the line.
+    cout << line << endl;    // Output the line.
+    }
+    return 0;
+}
