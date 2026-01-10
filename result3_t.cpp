bbf60c9a2bc751a4945b998314b2727134435c09
Currents <currentsaisyah@gmail.com>
2026-01-10 14:34:13 +0700

    chapter 15

---
+++ result3_t.cpp
@@ -0,0 +1,21 @@
+#include "result3.h"
+#include <iostream>
+using namespace std;
+int main()         //Some air temperature measurements
+{
+DayTime  morning(6,45);
+Result temp1( 6.45, morning),
+temp2( 11.2, 12,0,0);
+double temp = 0.0;
+cout << "\nWhat is the air temperature now? ";
+cin >> temp;
+Result temp3(temp);           // At current time.
+cout << "\n Temperature   Time  \n"
+<< "-------------------------" << endl;
+temp1.print();  temp2.print();  temp3.print();
+cout
+<< "\n Minimum Temperature: " << Result::getMin()
+<< "\n Maximum Temperature: " << Result::getMax()
+<< endl;
+return 0;
+}
