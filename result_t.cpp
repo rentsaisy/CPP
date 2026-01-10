e9ba410e0add1c970a79f23cf97b313f98c18553
Currents <currentsaisyah@gmail.com>
2026-01-10 14:26:23 +0700

    chapter 15

---
+++ result_t.cpp
@@ -0,0 +1,19 @@
+#include "Result.h"
+#include <iostream>
+using namespace std;
+int main()        // Some air temperature measurements
+{
+DayTime  morning(6,30);
+Result t1,                // Default constructor
+t2( 12.5, morning),
+t3( 18.2, 12,0,0),
+t4(17.7);          // at current time
+cout << "Default values: ";   t1.print();
+cout << "\n Temperature   Time  \n"
+<< "-------------------------" << endl;
+t2.print();
+t3.print();
+t4.print();
+cout << endl;
+return 0;
+}
